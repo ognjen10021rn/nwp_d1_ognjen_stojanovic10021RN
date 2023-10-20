3f29ae38d3733f287555caf9d1ab7a807da0c490
ognjen10021rn <ostojanovic10021rn@raf.rs>
2023-10-20 17:23:18 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'nwp_d1_ognjen_stojanovic_10021rn';
+}
